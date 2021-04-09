dd8068700fc50c2189911a1e324ed76227fc19b5
Chip <80671878+023-MasterChip@users.noreply.github.com>
2021-04-09 21:35:57 +0530

    Sound + highscore

--- Assets/Scripts/playerFollow.cs
+++ Assets/Scripts/playerFollow.cs
@@ -7,11 +7,13 @@ public class playerFollow : MonoBehaviour
 {
     public int score;
     public Text scoreText;
+    public Text highscore;
     // Start is called before the first frame update
     void Start()
     {
         Cursor.visible = false; // Cursor disabled
         score = 0; // initial score value (!!!Dont change)
+        highscore.text = PlayerPrefs.GetInt("highscore", 0).ToString();
     }
 
     // Update is called once per frame
@@ -24,8 +26,15 @@ public class playerFollow : MonoBehaviour
     // Adding score 
     public void ScorePoints(int pointToAdd)
     {
-        Debug.Log("Score added"); 
+        Debug.Log("Score added");
         score += pointToAdd;
         scoreText.text = "" + score;
+        scoreText.text = score.ToString();//highscore
+        if (score > PlayerPrefs.GetInt("highscore", 0))
+        {
+            PlayerPrefs.SetInt("highscore", score);
+            //nthekyo chyth last ayiii,player prf to store
+            highscore.text = score.ToString();
+        }
     }
 }
